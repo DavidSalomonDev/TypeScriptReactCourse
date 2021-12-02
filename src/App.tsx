817af02f1374ad2a265434d0467e03a9d087c7b9
David ChromeOS <me@davidsalomon.dev>
2021-12-02 09:47:29 -0600

    ✨ basic and advanced props typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Greet from "./components/Greet";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
+import Status from "./components/Status";
+import Heading from "./components/Heading";
+import Oscar from "./components/Oscar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const personName = {
+		first: "David",
+		last: "Salomon",
+	};
+
+	const nameList = [
+		{
+			first: "Julio",
+			last: "Son",
+		},
+		{
+			first: "Will",
+			last: "Smith",
+		},
+		{
+			first: "Roger",
+			last: "Federer",
+		},
+	];
+
+	return (
+		<div className="App">
+			<Greet name="David" messageCount={11} isLoggedIn={false} />
+			<Person name={personName} />
+			<PersonList names={nameList} />
+			<hr />
+			<Status status="success" />
+			<Heading>Placeholder text</Heading>
+			<Oscar>
+				<Heading>This is Oscar</Heading>
+			</Oscar>
+			<Greet name="Robert" isLoggedIn={true} />
+		</div>
+	);
 }
 
 export default App;
